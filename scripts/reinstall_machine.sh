0d85477b6e6a5670697cc957b265623155aaded3
Tomasz Domagała <tomasz.domagala.contact@gmail.com>
2021-05-27 20:01:03 +0200

    A and B tasks

--- scripts/reinstall_machine.sh
+++ scripts/reinstall_machine.sh
@@ -23,20 +23,9 @@ fi
 done
 
 ssh -p "${ssh_port}" root@localhost << EOF
-cd /usr/src
-make includes
-cd /usr/src/minix/fs/procfs
-make && make install
-cd /usr/src/minix/servers/pm
-make && make install
-cd /usr/src/minix/drivers/storage/ramdisk
-make && make install
-cd /usr/src/minix/drivers/storage/memory
-make && make install
-cd /usr/src/lib/libc
-make && make install
-cd /usr/src/releasetools
-make do-hdboot
+cd /usr/src/minix/fs/mfs || exit 1
+make || exit 1
+make install || exit 1
 
 echo "Rebooting. You can exit with Ctrl+C"
 reboot
